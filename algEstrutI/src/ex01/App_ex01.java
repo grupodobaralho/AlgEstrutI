8493da14c45c629a3f6f8d3c84675d5726ca5637
Israel Deorce <israel.idvj@gmail.com>
2016-09-25 02:37:53 -0300

    ex1 iniciado

--- algEstrutI/src/ex01/App_ex01.java
+++ algEstrutI/src/ex01/App_ex01.java
@@ -1,9 +1,15 @@
 package ex01;
 
+/* Descreva as alterações necessárias que deveriam ser feitas nos métodos
+ * remove e removeByIndex da classe ListArrayOfInteger para que o arranjo 
+ * interno de elementos seja reduzido para metade do tamanho toda vez que
+ * o número de itens da lista baixar de N/4 (onde N é o número de elementos
+ * da lista).
+ */
 public class App_ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
