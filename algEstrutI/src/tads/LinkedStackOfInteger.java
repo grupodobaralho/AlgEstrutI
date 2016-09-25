bdcfe8419cbcb015075c34a64fa68442354ddf95
Israel Deorce <israel.idvj@gmail.com>
2016-09-25 02:11:39 -0300

    LinkedStackOfInteger implementado
    revisem plox

--- algEstrutI/src/tads/LinkedStackOfInteger.java
+++ algEstrutI/src/tads/LinkedStackOfInteger.java
@@ -32,21 +32,64 @@ public class LinkedStackOfInteger {
 		if(count==0){
 			head = n;
 			tail=head;
+			count++;
 		}
 		else{		
 			Node aux = tail;
 			tail=n;
 			aux.next = tail;
+			count++;
 		}
 	}
 	
+	public Integer pop(){
+		if(count==0)
+			throw new IndexOutOfBoundsException("Cannot pop, stack is empty.");
+		Integer aux = tail.element;
+		if(count==1){			
+			head = null;
+			tail = null;
+			count = 0;
+			return aux;			 
+		}		
+		Node n = head;
+		for(int i=1; i<count-1; i++){
+			n = n.next;		
+		}
+		tail=n;
+		count--;
+		return aux;		
+	}
 	
+	public int size(){
+		return count;
+	}
 	
+	public boolean isEmpty(){
+		return count == 0;
+	}
 	
+	public void clear(){
+		head = null;
+		tail = null;
+		count = 0;
+	}
 	
+	public Integer top(){
+		if(count==0)
+			throw new IndexOutOfBoundsException("Cannot pop, stack is empty.");
+		return tail.element;
+	}
 	
-	
-	
-	
-	
+	//toString
+	@Override
+	public String toString() {
+		String aux = "";
+		Node n = head;
+		for(int i=0; i<count; i++) {
+			aux += (n.element + "\n");
+			n = n.next;
+		}
+		return aux;
+	}		
 }
