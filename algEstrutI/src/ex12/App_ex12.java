8909ed550652ecca1105e136a4e8eaa8369a20ce
Israel Deorce <israel.deorce@acad.pucrs.br>
2016-09-26 13:13:41 -0300

    Exercício 12 concluído

--- algEstrutI/src/ex12/App_ex12.java
+++ algEstrutI/src/ex12/App_ex12.java
@@ -1,10 +1,34 @@
 package ex12;
 
+//Israel:
+import tads.ArrayStackOfInteger;
+
 public class App_ex12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayStackOfInteger stack1 = new ArrayStackOfInteger();
+		ArrayStackOfInteger stack2 = new ArrayStackOfInteger();
+		
+		//1 representa J
+		//2 representa A
+		//3 representa V
+		//4 representa A
+		
+		stack1.push(1);	
+		stack1.push(2);
+		stack1.push(3);
+		stack1.push(4);
+		
+		stack2.push(stack1.pop());
+		stack2.push(stack1.pop());
+		stack2.push(stack1.pop());
+		stack2.push(stack1.pop());
+		
+		System.out.println(stack2.pop());
+		System.out.println(stack2.pop());
+		System.out.println(stack2.pop());
+		System.out.println(stack2.pop());
+		
 	}
-
+		
 }
